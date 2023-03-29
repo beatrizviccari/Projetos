281bebe90089baa0b7171d4c21a63f7935d84b9f
beatrizviccari <123665293+beatrizviccari@users.noreply.github.com>
2023-03-29 15:59:52 -0300

    Add files via upload

---
+++ paginar.js
@@ -0,0 +1,9 @@
+function paginar(idPagina) {
+  const elementoAtivo = document.querySelector(".item.active");
+  elementoAtivo.classList.remove("active");
+
+  const novaPagina = document.getElementById(idPagina);
+  novaPagina.classList.add("active");
+}
+	
+		
